fb825fce10955e084dc3cbe5bb102a2a44c25f6d
Arsal Zaman <55061545+arsalz1999@users.noreply.github.com>
2019-11-08 18:42:54 +0000

    Submission for formative

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 #include <sstream>
 #include <iostream>
 
+//program takes in binary in the command prompt
+
 int main(int argc, char* argv[]){
 
     std::string name_bin;
     if(argc>1) name_bin = argv[1];
     else std::exit(-21);
-    
+
     compiler comp(name_bin);
     comp.run();
 
